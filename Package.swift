9c1ee748ef77112d02ba1036586d11a3cee4ee0e
Homeism <119736356+Homeism@users.noreply.github.com>
2021-05-27 18:26:52 +0000

    Bumps version

---
+++ Package.swift
@@ -0,0 +1,10 @@
+
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CryptoKit25519",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
