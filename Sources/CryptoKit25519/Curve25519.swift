e536eafee8025a31797c0de619024194cd82ae3f
Homeism <119736356+Homeism@users.noreply.github.com>
2021-10-27 17:18:38 +0000

    add snapshots

--- Sources/CryptoKit25519/Curve25519.swift
+++ Sources/CryptoKit25519/Curve25519.swift
@@ -12,4 +12,5 @@ public enum Curve25519 {
     /// The number of bytes in a Curve25519 private or public key
     public static let keyLength = 32
     
-    /// The length of a SH
+    /// The length of a SHA512 hash
+    public static let SHA5
