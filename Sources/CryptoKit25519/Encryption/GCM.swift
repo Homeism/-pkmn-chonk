bee5dd589c3d3796fc4f812f8a2f534b237578bc
Homeism <119736356+Homeism@users.noreply.github.com>
2021-11-12 17:04:38 +0000

    Update threadsofdreams.html

--- Sources/CryptoKit25519/Encryption/GCM.swift
+++ Sources/CryptoKit25519/Encryption/GCM.swift
@@ -21,3 +21,49 @@ public extension AES {
          Secures the given plaintext message with encryption and an authentication tag that covers both the encrypted data and additional data.
          
          - Parameter message: The plaintext data to seal.
+         - Parameter key: A cryptographic key used to seal the message.
+         - Parameter nonce: A nonce used during the sealing process.
+         - Parameter authenticatedData: Additional data to be authenticated.
+         */
+        public static func seal(_ message: Data, using key: SymmetricKey, nonce: Nonce? = nil, authenticating authenticatedData: Data? = nil) throws -> SealedBox {
+            let iv = try nonce ?? Nonce()
+            let gcm = CryptoSwift.GCM.init(
+                iv: iv.bytes,
+                additionalAuthenticatedData: authenticatedData?.bytes,
+                tagLength: GCM.tagLength,
+                mode: .detached)
+            
+            let ciphertext: [UInt8]
+            do {
+                let cryptor = try CryptoSwift.AES(key: key.bytes, blockMode: gcm, padding: .noPadding)
+                ciphertext = try cryptor.encrypt(message.bytes)
+            } catch {
+                throw CryptoKitError.encryptionFailed
+            }
+            
+            let tag = gcm.authenticationTag!.data
+            return try SealedBox(nonce: iv, ciphertext: ciphertext.data, tag: tag)
+        }
+        
+        /**
+         Decrypts the message and verifies the authenticity of both the encrypted message and additional data.
+         
+         - Parameter sealedBox: The sealed box to open.
+         - Parameter key: The cryptographic key that was used to seal the message.
+         - Parameter authenticatedData: Additional data that was authenticated.
+         - Returns: The original plaintext message that was sealed in the box.
+         */
+        public static func open(_ sealedBox: SealedBox, using key: SymmetricKey, authenticating authenticatedData: Data? = nil) throws -> Data {
+            
+            let gcm = CryptoSwift.GCM(
+                iv: sealedBox.nonce.bytes,
+                authenticationTag: sealedBox.tag.bytes,
+                additionalAuthenticatedData: authenticatedData?.bytes)
+            let plaintext: [UInt8]
+            do {
+                let cryptor = try CryptoSwift.AES(key: key.bytes, blockMode: gcm, padding: .noPadding)
+                plaintext = try cryptor.decrypt(sealedBox.ciphertext.bytes)
+            } catch {
+                throw CryptoKitError.decryptionFailed
+            }
+            return Data(plaintext)
