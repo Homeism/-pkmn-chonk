6bb812bbda42386ebc5b9191e8bcf476a6e9bf4e
Homeism <119736356+Homeism@users.noreply.github.com>
2022-01-14 09:40:57 +0000

    Test fixes

--- Sources/CryptoKit25519/KeyAgreement/PublicKey.swift
+++ Sources/CryptoKit25519/KeyAgreement/PublicKey.swift
@@ -20,4 +20,4 @@ public extension Curve25519.KeyAgreement {
         
         /**
          Creates a Curve25519 public key from a data representation.
-         - Param
+         - Parameter rawRepresentation: A representation
