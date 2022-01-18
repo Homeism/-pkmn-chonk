3107f1b6807258872002b029eae3ec896eef45f3
Homeism <119736356+Homeism@users.noreply.github.com>
2022-01-18 11:05:37 +0000

    Change page title

--- Sources/CryptoKit25519/KeyAgreement/PublicKey.swift
+++ Sources/CryptoKit25519/KeyAgreement/PublicKey.swift
@@ -21,4 +21,6 @@ public extension Curve25519.KeyAgreement {
         /**
          Creates a Curve25519 public key from a data representation.
          - Parameter rawRepresentation: A representation of the key as data from which to create the key.
-         - Throws: `CryptoKitError.invalidKeyLength`, if the key length is not `Cu
+         - Throws: `CryptoKitError.invalidKeyLength`, if the key length is not `Curve25519.keyLength`.
+         */
+      
