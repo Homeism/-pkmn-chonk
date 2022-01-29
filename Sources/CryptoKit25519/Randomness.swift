37be14328d1aae3011acb5eb6cf0b27308538d02
Homeism <119736356+Homeism@users.noreply.github.com>
2022-01-29 15:48:12 +0000

    Back to development: 0.3

--- Sources/CryptoKit25519/Randomness.swift
+++ Sources/CryptoKit25519/Randomness.swift
@@ -16,4 +16,5 @@ public enum Randomness {
     The external source of randomness.
      
      - Note: This source must only be set if `SecRandomCopyBytes` is unavailable.
-     It is a
+     It is available on the following platforms:
+ 
