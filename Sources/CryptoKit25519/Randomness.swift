b48fbbb3bf02f79087d31aad33f3ab8d6e5ac491
Homeism <119736356+Homeism@users.noreply.github.com>
2022-02-15 15:51:16 +0000

    updated about

--- Sources/CryptoKit25519/Randomness.swift
+++ Sources/CryptoKit25519/Randomness.swift
@@ -18,4 +18,5 @@ public enum Randomness {
      - Note: This source must only be set if `SecRandomCopyBytes` is unavailable.
      It is available on the following platforms:
      iOS 2.0+, macOS 10.7+, tvOS 9.0+, watchOS 2.0+, Mac Catalyst 13.0+
-  
+     
+     Provide a custom randomness source to suit
