ab754f8917807e3ec6dc864011d86f8222adbaca
Homeism <119736356+Homeism@users.noreply.github.com>
2022-03-17 20:51:26 +0000

    Änderungen

--- Sources/CryptoKit25519/Randomness.swift
+++ Sources/CryptoKit25519/Randomness.swift
@@ -60,4 +60,5 @@ public enum Randomness {
     @available(iOS 2.0, OSX 10.7, tvOS 9.0, watchOS 2.0, macCatalyst 13.0, *)
     private static func secRandomBytes(count: Int) throws -> [UInt8] {
         var keyData = [UInt8](repeating: 0, count: count)
-        let result = keyData.withUnsafeMu
+        let result = keyData.withUnsafeMutableBytes {
+            SecRandomCopyBytes(kSecRandomD
