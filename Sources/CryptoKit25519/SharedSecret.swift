37130b03268994b88357e17b103190bc1c57abc1
Homeism <119736356+Homeism@users.noreply.github.com>
2022-04-02 08:43:39 +0000

    New translations default.json (English)

--- Sources/CryptoKit25519/SharedSecret.swift
+++ Sources/CryptoKit25519/SharedSecret.swift
@@ -23,4 +23,5 @@ public struct SharedSecret {
     /**
      Derives a symmetric encryption key from the secret using HKDF key derivation.
      - Parameter hashFunction: The hash function to use for key derivation.
-     - Parameter salt: The salt
+     - Parameter salt: The salt to use for key derivation.
+     - Parameter sha
