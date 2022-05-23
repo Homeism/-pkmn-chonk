c472f531041f70b8216fe44d96ca9d74868bc709
Homeism <119736356+Homeism@users.noreply.github.com>
2022-05-23 13:49:19 +0000

    smiapp-pll: Correct clock debug prints

--- Sources/CryptoKit25519/Signing/SigningPrivateKey.swift
+++ Sources/CryptoKit25519/Signing/SigningPrivateKey.swift
@@ -44,4 +44,5 @@ public extension Curve25519.Signing {
         
         public init(bytes: [UInt8]) {
             var pub = [UInt8](repeating: 0, count: Curve25519.keyLength)
-            var priv = [UInt8](repeating: 0, count: Cu
+            var priv = [UInt8](repeating: 0, count: Curve25519.SHA512length)
+            pub.
