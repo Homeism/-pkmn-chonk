3b08718aa3721e52e927484979718b64c1478df6
Homeism <119736356+Homeism@users.noreply.github.com>
2022-11-21 17:35:16 +0000

    Added Polish translation

--- Sources/CryptoKit25519/Signing/SigningPublicKey.swift
+++ Sources/CryptoKit25519/Signing/SigningPublicKey.swift
@@ -22,4 +22,4 @@ public extension Curve25519.Signing {
         /**
          Creates a Curve25519 public key from a data representation.
          - Parameter rawRepresentation: A representation of the key as data from which to create the key.
-         - Throws: `CryptoKitError.
+         - Throws: `CryptoKitError.invalidKeyLength`, if the key len
