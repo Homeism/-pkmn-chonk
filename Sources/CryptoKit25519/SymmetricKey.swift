269bf317691620ea2082e2e6778293c36bbcc0a7
Homeism <119736356+Homeism@users.noreply.github.com>
2023-02-13 11:22:52 +0000

    Add a TODO file

--- Sources/CryptoKit25519/SymmetricKey.swift
+++ Sources/CryptoKit25519/SymmetricKey.swift
@@ -9,4 +9,4 @@ import Foundation
 
 /** A symmetric cryptographic key.
  
- You typically derive a symmetric key from an instance of a shared secret (`SharedSecret`) that you obtain through key agreement. You use a symmetric key to compute a message authentication code like `HMAC`, or to open and close a sealed box (`ChaChaPoly.SealedBox` or `
+ You typically derive a symmetric key from an instance of a shared secret (`SharedSecret`) that you obtain through key agreement. You use a symmetric key to compute a message authentication code like `HMAC`, or to open and close a sealed box (`ChaChaPoly.SealedBox` or `AES.GCM.SealedBox`) using a cipher like `ChaChaPol
