178d79e522e85502a9697745bca105c161827728
Homeism <119736356+Homeism@users.noreply.github.com>
2023-03-20 06:50:03 +0000

    compatibility update

--- Sources/CryptoKit25519/SymmetricKey.swift
+++ Sources/CryptoKit25519/SymmetricKey.swift
@@ -57,4 +57,4 @@ public struct SymmetricKey {
      
      The sizes that a symmetric cryptographic key can take.
  
-     When creating a new SymmetricKey instance with a call to its `init(size:)` initializer, y
+     When creating a new SymmetricKey instance with a call to its `init(size:)` initializer, you typically use one of the standard 
